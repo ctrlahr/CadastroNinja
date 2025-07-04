4682f78f1efa5ea89afe7eedcac6ffcf1d267b0a
Jorge Luís <134817598+ctrlahr@users.noreply.github.com>
2025-07-04 16:32:51 -0300

    I dont know what i change

--- src/main/java/JorgeLuis/CadastroNinja/Missoes/MissoesModel.java
+++ src/main/java/JorgeLuis/CadastroNinja/Missoes/MissoesModel.java
@@ -17,7 +17,7 @@ public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String nome;
 
     private String dificuldade;
