31bcc5f3ac7edd67defb5f59815a9fc9c1c72add
ctrlahr <ctrlahrium@gmail.com>
2025-07-11 12:25:31 -0300

    fix: fix migrations of flyway and create a new migrate to create Naruto ninja

---
+++ src/main/resources/db/Migrations/V2__ADD_naruto_tb_cadastro.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_cadastro(id, email, idade, img_url, nome, missoes_id)
+VALUES(1, 'narutinho@email.com', 16, 'naruto.png', 'Naruto Uzumaki', 1)
--- src/main/resources/db/Migrations/V2__Add_Rank_tb_cadastro.sql
+++
@@ -1,4 +0,0 @@
--- V2: Migration para adicionar a coluna de RANK na tabela de cadastro.
-ALTER TABLE TB_CADASTRO
-ADD COLUMN missoes_concluidas INTEGER;
-
